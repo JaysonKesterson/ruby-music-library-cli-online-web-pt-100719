1194bc00855c249a1a9bfbe589e9b2b0b61cbe73
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-15 17:43:57 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -21,16 +21,41 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort{|a,b| a.name <=> b.name}.each_with_index do |song,index|
+    Song.all.sort_by(&:name).each_with_index do |song,index|
       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
   
   def list_artists
-    Artist.all.sort{|a,b| a.name <=> b.name}.each_with_index do |artist,index|
+    Artist.all.sort_by(&:name).each_with_index do |artist,index|
       puts "#{index + 1}. #{artist.name}"
     end
   end
   
+  def list_genres
+    Genre.all.sort_by(&:name).each_with_index do |artist,index|
+      puts "#{index + 1}. #{artist.name}"
+    end
+  end
+    
+  def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      input = gets.strip
+      if artist = Artist.find_by_name(input)
+      artist.songs.sort_by(&:name).each_with_index do |song,index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+  
+  def list_songs_by_genre
+     puts "Please enter the name of a genre:"
+      input = gets.strip
+      if genre = Genre.find_by_name(input)
+      genre.songs.sort_by(&:name).each_with_index do |genre,index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
   
 end
