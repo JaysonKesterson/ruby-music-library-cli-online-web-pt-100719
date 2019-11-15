cf898d25f9bb55b35e0f1d1a1e3662213b1b120e
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-15 18:55:16 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -61,6 +61,6 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets
-    if list_songs.
+    if list_songs.include?(input)
   end
 end
